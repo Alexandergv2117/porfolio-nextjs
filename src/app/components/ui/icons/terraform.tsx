bdc6663f47cb69b42b6cb206f15266fcc06b5478
Alexander Garcia <alexandergv2117@gmail.com>
2024-09-19 22:56:00 -0500

    Update SVG attributes to adhere to camel case convention

--- src/app/components/ui/icons/terraform.tsx
+++ src/app/components/ui/icons/terraform.tsx
@@ -13,11 +13,11 @@ export default function Terraform(props: SVGProps<SVGSVGElement>) {
       {...props}
     >
       <title className="tooltip-text">Terraform</title>
-      <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
       <g
         id="SVGRepo_tracerCarrier"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       ></g>
       <g id="SVGRepo_iconCarrier">
         {" "}
