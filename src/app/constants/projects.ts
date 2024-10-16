926b19cdc69643482c99975a709773daf144f91f
Alexander Garcia <alexandergv2117@gmail.com>
2024-10-15 22:36:32 -0500

    feat: add el epoch projects

--- src/app/constants/projects.ts
+++ src/app/constants/projects.ts
@@ -14,13 +14,30 @@ interface Project {
 
 export const PROJECTS: Project[] = [
   {
+    title: "El epoch",
+    company: "The Epoch Times México",
+    href: "https://elepoch.com/",
+    tags: ["work project"],
+    description: [
+      "Desarrolle la página web del periódico the epoch times méxico, migrandola de WordPress a Next.js y TailwindCSS. Se implementó Server Side Rendering (SSR) para mejorar el SEO y la velocidad de carga.",
+    ],
+    image: "/el_epoch.webp",
+    stack: [
+      ICONS_NAMES.TypeScript,
+      ICONS_NAMES.NextJS,
+      ICONS_NAMES.TailwindCSS,
+      ICONS_NAMES.GH_Actions,
+      ICONS_NAMES.Docker,
+    ],
+  },
+  {
     title: "Elecciones USA 2024",
     company: "The Epoch Times México",
     href: "https://elecciones2024usa.theepochtimes.com/",
     tags: ["landing page", "work project"],
     description: [
       "Desarrollo de Landing Page para la Cobertura Especial de las Elecciones Presidenciales de USA 2024 - The Epoch Times México.",
-      "Codifiqué una landing page para la cobertura especial de las elecciones presidenciales de USA 2024, destinada a The Epoch Times México. Utilicé Next.js y TailwindCSS, implementando Static Site Generation (SSG) para optimizar el rendimiento y consumiendo datos de una API de WordPress. También configuré GitHub Actions para el despliegue automático, realizado con Docker Swarm y Caddy.",
+      "Utilicé Next.js y TailwindCSS, implementando Static Site Generation (SSG) para optimizar el rendimiento y consumiendo datos de una API de WordPress. También configuré GitHub Actions para el despliegue automático, realizado con Docker Swarm y Caddy.",
     ],
     image: "/elecciones2024USA.webp",
     stack: [
