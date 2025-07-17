e5cf139244a39e4ee82e22050ecd1628af829090
Alexander Garcia <alexandergv2117@gmail.com>
2025-07-17 11:25:05 -0500

    config next for render md pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,16 @@
+import createMDX from '@next/mdx';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone",
-  images: {
-    domains: ['avatars.githubusercontent.com'],
-  },
+   output: 'standalone',
+   images: {
+      domains: ['avatars.githubusercontent.com'],
+   },
+   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
 };
 
-export default nextConfig;
+const withMDX = createMDX({
+   extension: /\.(md|mdx)$/
+});
+
+export default withMDX(nextConfig);
