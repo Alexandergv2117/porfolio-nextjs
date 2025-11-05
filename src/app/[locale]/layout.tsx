5281096f031ee88bd4532989560854e2e680cbcd
Alexander Garcia <74172014+Alexandergv2117@users.noreply.github.com>
2025-11-05 15:50:13 -0500

    feat: add Microsoft Clarity script for enhanced analytics tracking

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import localFont from "next/font/local";
 import { GoogleAnalytics } from "@next/third-parties/google";
 
@@ -54,6 +55,18 @@ export default async function RootLayout({
           {children}
           <Footer />
         </NextIntlClientProvider>
+        {/* 👇 Script de Microsoft Clarity */}
+        {NODE_ENV === "production" && (
+          <Script id="microsoft-clarity" strategy="afterInteractive">
+            {`
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "u1j1jpvcbb");
+            `}
+          </Script>
+        )}
       </body>
       {NODE_ENV === "production" && <GoogleAnalytics gaId="G-VNQRKJDSSE" />}
     </html>
