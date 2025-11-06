a10150f1c1619eadb7cb70ac1b132715f50fb8d4
Alexander Garcia <74172014+Alexandergv2117@users.noreply.github.com>
2025-11-06 11:19:29 -0500

    feat: add Google Tag Manager script for enhanced tracking

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -47,6 +47,26 @@ export default async function RootLayout({
 
   return (
     <html lang={locale} className="dark">
+      <head>
+        {/* Google Tag Manager */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){
+                w[l]=w[l]||[];
+                w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
+                var f=d.getElementsByTagName(s)[0],
+                    j=d.createElement(s), dl=l!='dataLayer'?'&l='+l:'';
+                j.async=true;
+                j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+                f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-55CTMHMV');
+            `,
+          }}
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased sm:max-w-[60rem] relative flex flex-col justify-center items-center mx-auto px-4 sm:px-2 pb-2`}
       >
